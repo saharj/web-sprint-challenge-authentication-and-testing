1cc74e711229646ff6bac7c3d6bf2dea46c3f029
Sahar Jafari <shr.jafari@gmail.com>
2020-11-13 08:31:39 -0800

    setup + register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const { jwtSecret } = require("./secrets.js");
+const { isValid } = require("./usersService");
+const User = require("../database/dbConfig");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hashed = bcrypt.hashSync(credentials.password, rounds);
+
+    credentials.password = hashed;
+    User.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password should be alphanumeric",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
